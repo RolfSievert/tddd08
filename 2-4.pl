8324cf9d53130b39fe82d94ac478dfd7c61a43c4
Sebastian Ragnarsson <sebra023@student.liu.se>
2018-09-20 15:54:22 +0200

    fixed 2-4 power

--- 2-4.pl
+++ 2-4.pl
@@ -5,8 +5,7 @@ union(S, [], U):-
     S\=[],
     U=S.
 union([], S, U):-
-    S\=[],
-    U=S.
+    union(S, [], U).
 union([H1|T1], [H2|T2], [UH|UT]):-
     H1@<H2,
     UH=H1,
@@ -20,28 +19,36 @@ union([H1|T1], [H2|T2], [UH|UT]):-
     
 intersection([], [], U):-
     U=[].
-intersection([H1|T1], [H2|T2], [UH|UT]):-
+intersection(S, [], U) :-
+    S\=[],
+    U=[].
+intersection([], S, U) :-
+    S\=[],
+    U=[].
+intersection([H1|T1], [H2|T2], U):-
     H1=H2,
+    U=[UH|UT],
     UH=H1,
     intersection(T1, T2, UT);
-    H1\=H2,
     (
         H1@<H2,
-        intersection(T1, [H2|T2], [UH|UT]);
+        intersection(T1, [H2|T2], U);
         H2@<H1,
-        intersection([H1|T1], T2, [UH|UT])
+        intersection([H1|T1], T2, U)
     ).
-insert(X, [], Z):-
-    Z=[].
+
+insert(X, [[]], Z):-
+    Z=[[X]].
 insert(X, [H|T], Z):-
-    [H|T]\=[],
+    [H|T]\=[[]],
     append([X], H, L),
     append([L], L1, Z),
     insert(X, T, L1).
+
 power([], U):-
     U=[[]].
 power([H|T], U):-
     [H|T]\=[],
-    insert(H, X, S),
     power(T, X),
+    insert(H, X, S),
     append(S, X, U).
