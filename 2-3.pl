cb278895feea529de3dda8df65c4a93a7e6629ae
RolfSievert <rolfrf@hotmail.com>
2018-09-24 10:20:38 +0200

    Filled in functions for 2-3, untested not working

--- 2-3.pl
+++ 2-3.pl
@@ -1,13 +1,39 @@
-% An Abstract Machine
+% Gets the value E of identifier I
+id(E, I, Sn):-
+    memberchk(I=E, Sn).
+
+% Does nothing
 execute(S0, skip, S0).
 
 % Assumes E is a Number to begin with
 execute(S0, set(I, E), Sn) :-
-	Sn = [I=E].
+    % If identifier already exists in S0
+    memberchk(I=A, S0),
+    delete(S0, I=A, S1),
+    sublist(S1, Sn),
+    memberchk(I=E, Sn);
+    % If it doesn't exist in S0
+    \+memberchk(I=_, S0),
+    sublist(S0, Sn),
+    memberchk(I=E, Sn).
 
-%execute(_S0, if(B, C, C), _Sn).
+% Executes C1 if B, else C2
+execute(S0, if(B, C1, C2), Sn):-
+    % if B is true
+    B,
+    execute(S0, C1, Sn);
+    % if B is false
+    \+B,
+    execute(S0, C2, Sn).
 
-%execute(_S0, while(B, C), _Sn).
+% Executes C while boolean B is true
+execute(S0, while(B, C), Sn):-
+    B,
+    execute(S0, C, S1),
+    execute(S1, while(B, C), Sn).
 
-%execute(_S0, seq(C, C), _Sn).
+% Executes C1 and C2 in that order
+execute(S0, seq(C1, C2), Sn):-
+    execute(S0, C1, S1),
+    execute(S1, C2, Sn).
 
