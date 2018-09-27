765a68707af9045bd3bb5dd0eaa4ffffe5e02407
RolfSievert <rolfrf@hotmail.com>
2018-09-27 09:13:49 +0200

    Fixed loading other prolog scripts

--- 3-1.pl
+++ 3-1.pl
@@ -1,14 +1,17 @@
 % exec 3.1
 
 % Load functors and predicates
-:-['scanner.pl]'].
-:-['3-1.pl'].
+:-initialization([scanner, '2-4']).
 
-parse(Tokens, AbstStx):-
+%parse(Tokens, AbstStx):-
 
 
 run(In, String, Out) :-
     scan(String, Tokens),
     write(Tokens).
-    parse(Tokens, AbstStx),
+%    parse(Tokens, AbstStx),
 %   execute(In, AbstStx, Out).
+
+
+% Test
+%:-run([x=3], "y:=1; z:=0; while x>z do z:=z+1; y:=y*z od", Res).
