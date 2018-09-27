130a4455cef5071a4d371f0519469e6c39bae207
RolfSievert <rolfrf@hotmail.com>
2018-09-27 10:03:43 +0200

    Added funtions to 3-1

--- 3-1.pl
+++ 3-1.pl
@@ -1,18 +1,23 @@
 % exec 3.1
 
 % Load functors and predicates
-:-initialization([scanner, '2-4']).
+:-initialization([scanner, '2-3']).
 
 % Set value
 parse([A, :=, B], set(A, B)).
+% Parse while loops
+parse([while|T], Out):-
+    append(Statement, do, T1),
+    append(T1, Task),
+    parse(Statement, Out1),
+    parse(Task, Out2),
+    append(Out1, Out2, Out).
 % Separate arguments by splitting lists on ';', 
 % execute both arguments sequentially
 parse(Tokens, seq(Out1, Out2)):-
     append(A, [;|B], Tokens),
     parse(A, Out1),
     parse(B, Out2).
-
-parse(Tokens, AbstStx):-
 
 
 run(In, String, Out) :-
