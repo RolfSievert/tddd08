77ea9af657448e2dec570078cda685df38f64753
RolfSievert <rolfrf@hotmail.com>
2018-09-29 14:15:57 +0200

    Added nontested code

--- 3-1.pl
+++ 3-1.pl
@@ -14,37 +14,66 @@
 %parse([A, -, B], A-B).
 %parse([A, +, B], A+B).
 %parse([A], A).
+% Remove ; if needed
+% Comparators
+comp(<).
+comp(>).
+parse([;|T], Out):-
+    parse(T, Out).
+
+% term
 parse([id(X)], id(X)).
 parse([num(X)], num(X)).
+
+% Factor
+parse(F, Out):-
+    append(T1, Fac, F),
+    append(Term, [+], T1),
+    parse(Term, PTerm),
+    parse(Fac, PFac).
+
+% Expr
+parse(E, Out):-
+    append(T, Expr, E),
+    append(Fac, [*], T),
+    parse(Fac, PFac),
+    parse(Term, PTerm).
+
+% Bool
 parse(Bool, P1<P2):-
     append(T, Expr2, Bool),
     append(Expr1, [<], T),
     parse(Expr1, P1),
     parse(Expr2, P2).
+parse(Bool, P1>P2):-
+    append(T, Expr2, Bool),
+    append(Expr1, [>], T),
+    parse(Expr1, P1),
+    parse(Expr2, P2).
 
-%parse([], skip).
-%parse([;|T], Out):-
-%    parse(T, Out).
-%parse([if|T], seq(if(PBool, PPgmTrue, PPgmFalse), PRest)):-
-%    append(Bool, [then], T1),
-%    append(T1, PgmTrue, T2),
-%    append(T2, [else], T3),
-%    append(T3, PgmFalse, T4),
-%    append(T4, fi, T5),
-%    append(T5, Rest, T),
-%    parse(Bool, PBool),
-%    parse(PgmTrue, PPgmTrue),
-%    parse(PgmFalse, PPgmFalse),
-%    parse(Rest, PRest).
-%parse([while|T], seq(while(PBool, PPgm), PRest)):-
-%    append(Bool, [do], T1),
-%    append(T1, Pgm, T2),
-%    append(T2, [od], T3),
-%    append(T3, Rest, T),
-%    parse(Bool, PBool),
-%    parse(Pgm, PPgm),
-%    parse(Rest, PRest).
-        
+% cmd
+parse([skip], skip).
+parse(Set, set(PId, PErxpr)):-
+    append(T, Expr, Set),
+    append(Id, [:=], T),
+    parse(Id, PId),
+    parse(Expr, PExpr).
+parse([if|T], if(PBool, PPgmTrue, PPgmFalse)):-
+    append(T4, fi, T),
+    append(T3, PgmFalse, T4),
+    append(T2, [else], T3),
+    append(T1, PgmTrue, T2),
+    append(Bool, [then], T1),
+    parse(Bool, PBool),
+    parse(PgmTrue, PPgmTrue),
+    parse(PgmFalse, PPgmFalse).
+% pgm
+parse(P, seq(PCmd, PPgm)):-
+    append(T, Pgm, P),
+    append(Cmd, [;], T),
+    parse(Cmd, PCmd),
+    parse(Pgm, PPgm).
+
 
 run(_In, String, Out) :-
     scan(String, Tokens),
