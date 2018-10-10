31da582be025493cc37beeeb47e107595f15ec3e
RolfSievert <rolfrf@hotmail.com>
2018-10-10 14:59:39 +0200

    Learned a bit about dynamic predicates

--- 5-1.pl
+++ 5-1.pl
@@ -2,18 +2,36 @@
 
 :- use_module(library(clpfd)).
 
+% Define dynamic predicates
+dynamic on/2.
+dynamic container/3.
+
 on(a,d).
 on(b,c).
 on(c,d).
 
+container(a,2,2).
+container(b,4,1).
+container(c,2,2).
+container(d,1,1).
+
+earliest_start(Box, Time):-
+    \+on(_, Box),
+    container(Box, _, Time).
+earliest_start(Box, Time):-
+    on(X, Box),
+    container(X, _, D),
+    Time #= D + T,
+    earliest_start(X, T).
+
 schedule(Starts) :-
         Starts = [S1,S2,S3,S4],
-        Tasks = [task(S1,2,_,2,_),
-                 task(S2,1,_,4,_),
-                 task(S3,2,_,2,_),
-                 task(S4,1,_,1,_)],
+        Tasks = [task(S1,2,_,2,a),
+                 task(S2,1,_,4,b),
+                 task(S3,2,_,2,c),
+                 task(S4,1,_,1,d)],
         %Starts ins 0..10,
-        cumulative(Tasks,_),
+        cumulative(Tasks, [limit(4)]),
         label(Starts).
     
 
@@ -26,9 +44,9 @@ schedule(Starts) :-
 
 
 
-%moveBox(OldFact, NewFact) :-
-%    call(OldFact),
-%    retract(OldFact),
-%    assertz(NewFact).
+unload_box(Box) :-
+    \+on(_, Box),
+    retract(on(Box, _),
+    retract(container(Box, _, _)).
 
 
