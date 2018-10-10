8a274fb221707d0e2b0af195aa074b06e17d1579
RolfSievert <rolfrf@hotmail.com>
2018-10-10 09:22:27 +0200

    Did some structure work on 4-1

--- 4-1.pl
+++ 4-1.pl
@@ -1,5 +1,6 @@
 % excersice 4.1
 
+%%%%% Functors for building graph
 % Returns number of missionaries in list M
 missionaries([], []).
 missionaries([m|T], [m|C]):-
@@ -20,22 +21,34 @@ not_outnumbered(R):-
     length(R, L),
     2*M>=L.
 
-% Checks if two nodes are directly connected
-connected(R1, Boat, R2):-
+% Creates a list of neighbour states to a given state
+get_neighbours([R1, Boat, R2, Side], Neighbours).
+
+% Checks if an element is in list
+in_graph([Elem|L], Elem).
+in_graph([H|L], Elem):-
+    H\=Elem,
+    in_graph(L, Elem).
+
+% Maybe pass condition that element cant already be in list? Insert unique only?
+% Inserts all pairs of elements in list
+insert(Elem, [H|T], L).
+
+% Creates a graph for our problem with initial state R1, Boat, R2, Side
+create_graph([R1, Boat, R2, Side], Graph):-
+    get_neighbours([R1, Boat, R2, Side], Neighbours),
+    insert([R1, Boat, R2, Side], Neighbours, G1),
+    append(G1, G2, Graph),
+    % Create graph for all neighbours
+    create_graph(
 
 
-%connected([H|R1], [], [], R1Out, BoatOut, []):-
-%    length(
-
-% Retrieves all neighbours for one node
-neighbours([], Node, []).
-%neighbours([[Node, Node2]], Node, Out):-
-    
-
-% Calculates the shortest path between Start and Goal
-%breadth_first(R1, Boat, R2, Start, Goal, Path):-
-    
-
-
+%%%%%% Functors for search
+% True if N1 and N2 are directly connected
+connected(Graph, N1, N2).
+% Returns all directly connected nodes to Node
+neighbours(Graph, Node, Neighbours).
+% Breadth first algorithm
+breadth_first(Graph, Start, Goal, Path).
 
 % Data = [c, c, c, m, m, m]
