7d8c9edf763f664054614d6eed7e91593c937669
Rolf Sievert <rolsi701@student.liu.se>
2018-10-15 17:03:21 +0200

    Added starts and label

--- 5-1.pl
+++ 5-1.pl
@@ -16,22 +16,23 @@ container(b,4,1).
 container(c,2,2).
 container(d,1,1).
 
-create_tasks([], _, []).
-create_tasks([Box|Boxes], End, Tasks):-
-    create_tasks(Boxes, End, T2),
-    create_task(Box, End, T1),
-    append(T1, T2, Tasks).
+create_tasks([], [], _, []).
+create_tasks([Box|Boxes], Starts, End, Tasks):-
+    create_tasks(Boxes, S2, End, T2),
+    create_task(Box, S1, End, T1),
+    append(T1, T2, Tasks),
+    append(S1, S2, Starts).
 
-create_task(Box, _, []):-
+create_task(Box, [], _, []):-
     container(Box, _, _),
     on(_, Box).
-create_task(Box, E, [task(Start, Duration, End, Resources, Box)|Tasks]):-
+create_task(Box, [Start|Starts], E, [task(Start, Duration, End, Resources, Box)|Tasks]):-
     container(Box, Resources, Duration),
     \+on(_, Box),
     Start #>= E,
     findall(X, on(Box, X), Boxes),
     unload_box(Box),
-    create_tasks(Boxes, End, Tasks).
+    create_tasks(Boxes, Starts, End, Tasks).
     
 on_top(Box):-
     container(Box, _, _),
@@ -40,8 +41,10 @@ on_top(Box):-
 schedule(Tasks):-
     % Unloadable boxes
     findall(X, on_top(X), Boxes),
-    create_tasks(Boxes, 0, Tasks),
-    cumulative(Tasks, [limit(10)]).
+    Starts ins 0..100,
+    create_tasks(Boxes, Starts, 0, Tasks),
+    cumulative(Tasks, [limit(10)]),
+    label(Starts).
 
 unload_box(Box):-
     \+on(Box, _).
